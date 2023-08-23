8adab03c5bc222e9ef7d74a63d6fcdd2873e28ef
HoangMinhNg <ngminh3004@gmail.com>
2023-08-23 23:49:39 +0700

    lesson 4 first commit

---
+++ Lesson4/src/main/java/com/hoangminhng/lesson4/Lesson4Application.java
@@ -0,0 +1,14 @@
+package com.hoangminhng.lesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson4Application.class, args);
+        System.out.println("Hello");
+    }
+
+}
