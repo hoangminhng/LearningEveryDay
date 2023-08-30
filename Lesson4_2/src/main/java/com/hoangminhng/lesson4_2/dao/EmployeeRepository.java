3fa28dac6bf46017550ce1bec8ad258cfca63967
HoangMinhNg <ngminh3004@gmail.com>
2023-08-30 15:30:41 +0700

    config and sorting

--- Lesson4_2/src/main/java/com/hoangminhng/lesson4_2/dao/EmployeeRepository.java
+++ Lesson4_2/src/main/java/com/hoangminhng/lesson4_2/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.hoangminhng.lesson4_2.dao;
 
 import com.hoangminhng.lesson4_2.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")//set the path from employees to members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //with JPaRepository, we no need to write any code
 }
