286d36ac97cbea68bb7842951326d00581b207ea
HoangMinhNg <ngminh3004@gmail.com>
2023-08-30 11:11:25 +0700

    done CRUD basic

--- Lesson4_2/src/main/java/com/hoangminhng/lesson4_2/rest/EmployeeRestController.java
+++ Lesson4_2/src/main/java/com/hoangminhng/lesson4_2/rest/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.hoangminhng.lesson4_2.rest;
 
 import com.hoangminhng.lesson4_2.entity.Employee;
 import com.hoangminhng.lesson4_2.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,39 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeID}")
+    public Employee getEmployee(@PathVariable int employeeID){
+        Employee theEmployee = employeeService.findByID(employeeID);
+
+        if (theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeID);
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeID}")
+    public String deleteEmploye(@PathVariable int employeeID){
+        Employee tempEmployee = employeeService.findByID(employeeID);
+        if (tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeID);
+        }
+        employeeService.deleteByID(employeeID);
+        return "Delete employee id - " + employeeID;
     }
 }
