8ef94f1cf65bbfd75f55ae334e2b56f6d3b33ea1
HoangMinhNg <ngminh3004@gmail.com>
2023-09-12 10:08:10 +0700

    first commit

---
+++ Lesson9/src/test/java/com/hoangminhng/lesson9/Lesson9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangminhng.lesson9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
