730f4de09943402befd6fb8dac0e29c084905c92
HoangMinhNg <ngminh3004@gmail.com>
2023-09-15 15:47:08 +0700

    config

---
+++ PresentationLayerDemo/src/main/java/com/hoangminhng/presentationlayerdemo/PresentationLayerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hoangminhng.presentationlayerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationLayerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentationLayerDemoApplication.class, args);
+    }
+
+}
---
+++ PresentationLayerDemo/src/test/java/com/hoangminhng/presentationlayerdemo/PresentationLayerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangminhng.presentationlayerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationLayerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
